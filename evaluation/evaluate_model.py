fixed rotation, config

evaluation/evaluate_model.py
@@ -5,7 +5,8 @@ import bob
 import numpy as np
 import pandas as pd
 import torch
-from torchvision.transforms.functional import to_tensor
+from torchvision.transforms import transforms, TenCrop, ToTensor
+from torchvision.transforms.functional import to_tensor, normalize
 from tqdm import tqdm
 
 from evaluation.charts import accuracy_table, generate_model_accuracy_of_testsets_2
@@ -27,33 +28,30 @@ class EvalModel(ModelManager):
         self.model_device.eval()
 
         per_attribute_accuracy_list = []
-        all_attribute_accuracy_list = []
 
         # loop through all files of folder
         test_folders = os.listdir(self.config.dataset.testsets_path)
         test_folders.sort()
-        # TODO: Remove
-        test_folders = ['testsetAM']
+        test_folders.remove('testsetC')
 
         for testset in test_folders:
             print("Calculating Accuracy for: {}".format(testset))
             test_folder = os.path.join(self.config.dataset.testsets_path, testset)
             image_names = [f for f in os.listdir(test_folder) if os.path.isfile(os.path.join(test_folder, f))]
             pbar = tqdm(range(len(image_names)))
-            correct_classifications = 0
             attributes_correct_count = torch.zeros(self.labels.shape[1])
             attributes_correct_count = attributes_correct_count.to(self.device)
 
-            batch_size = 32
+            batch_size = self.config.evaluation.batch_size
+
             image_name_batches = [image_names[i:i + batch_size] for i in range(0, len(image_names), batch_size)]
 
             for img_batch in image_name_batches:
                 inputs = []
                 labels = []
                 for img in img_batch:
-
                     # load image and label as tensors
-                    image, y = self._image_and_label_to_tensor(img, testset)
+                    image, y, _ = self._image_and_label_to_tensor(img, testset)
 
                     # Add image to inputs batch list
                     inputs.append(image)
@@ -80,15 +78,61 @@ class EvalModel(ModelManager):
                     outputs[outputs >= 0.5] = 1
 
                     attributes_correct_count += torch.sum(outputs == labels.type_as(outputs), dim=0)
-                    correct_classifications += torch.sum(outputs == labels.type_as(outputs))
                 pbar.update(len(img_batch))
 
             per_attribute_accuracy = attributes_correct_count / len(image_names)
             per_attribute_accuracy_list.append(per_attribute_accuracy.tolist())
-            all_attributes_accuracy = correct_classifications / (len(image_names) * self.labels.shape[1])
-            all_attribute_accuracy_list.append(all_attributes_accuracy.tolist())
-
             pbar.close()
+
+
+        # # validate 10 crop
+        # testset = 'testsetC'
+        # print("Calculating Accuracy for: {}".format(testset))
+        # test_folder = os.path.join(self.config.dataset.testsets_path, testset)
+        # image_names = [f for f in os.listdir(test_folder) if os.path.isfile(os.path.join(test_folder, f))]
+        # pbar = tqdm(range(len(image_names)))
+        # attributes_correct_count = torch.zeros(self.labels.shape[1])
+        # attributes_correct_count = attributes_correct_count.to(self.device)
+        # for img in image_names:
+        #     image, y, _ = self._image_and_label_to_tensor(img, testset)
+        #     transf = transforms.Compose([TenCrop(224)])
+        #     x = transf(image)
+        #
+        #     # turn list of tensors to 1 tensor of shape batch size C x H x W
+        #     inputs = torch.stack(x)
+        #
+        #     # turn list of tensors to 1 tensor of shape batch size x number of labels
+        #     labels = torch.stack([y]*10)
+        #
+        #     # inputs on GPU
+        #     inputs = inputs.to(self.device)
+        #
+        #     # labels on GPU
+        #     labels = labels.to(self.device)
+        #
+        #     # do not track history if inference
+        #     with torch.no_grad():
+        #         outputs = self.model_device(inputs)
+        #
+        #         # round percentages to 0 or 1
+        #         outputs[outputs < 0.5] = 0
+        #         outputs[outputs >= 0.5] = 1
+        #
+        #         # sum the outputs of the 10 crops
+        #         outputs_sum = torch.sum(outputs, dim=0)  # size = [1, ncol]
+        #
+        #         # take majority guess
+        #         outputs_sum[outputs_sum < 5] = 0
+        #         outputs_sum[outputs_sum >= 5] = 1
+        #
+        #         attributes_correct_count += torch.sum(outputs_sum == labels[0].unsqueeze(0), dim=0)
+        #     pbar.update(1)
+        #
+        # per_attribute_accuracy = attributes_correct_count / len(image_names)
+        # per_attribute_accuracy_list.append(per_attribute_accuracy.tolist())
+        #
+        # pbar.close()
+        # test_folders.append('testsetC')
 
         df = pd.DataFrame(np.transpose(per_attribute_accuracy_list), columns=test_folders)
         return df
@@ -125,14 +169,17 @@ class EvalModel(ModelManager):
 
         inputs = []
         labels = []
+        original_images = []
         for i in range(self.config.evaluation.qualitative.number_of_images_per_row * self.config.evaluation.qualitative.number_of_rows):
             img = image_names[i]
 
             # load image and label as tensors
-            image, y = self._image_and_label_to_tensor(img, self.config.evaluation.qualitative.testset_name)
+            image, y, original_img = self._image_and_label_to_tensor(img, self.config.evaluation.qualitative.testset_name)
 
             # Add image to inputs batch list
             inputs.append(image)
+
+            original_images.append(original_img)
 
             # Add label to labels batch list
             labels.append(y)
@@ -165,7 +212,7 @@ class EvalModel(ModelManager):
         per_attribute_correct_classification = per_attribute_correct_classification.cpu().tolist()
 
         accuracy_table_fig = accuracy_table(self.labels.columns.tolist(), prediction, per_attribute_correct_classification)
-        accuracy_sample = image_grid_and_accuracy_plot(images, accuracy_list,
+        accuracy_sample = image_grid_and_accuracy_plot(original_images, accuracy_list,
                                                        number_of_img_per_row=self.config.evaluation.qualitative.number_of_images_per_row,
                                       saveOnly=True)
 
@@ -173,32 +220,34 @@ class EvalModel(ModelManager):
 
         accuracy_table_fig.write_image(os.path.join(self.config.experiments_dir, 'acurracy_table.jpg'), scale=5)
 
-    def _image_and_label_to_tensor(self, img, testset):
-        # Load image from disk yields numpy array of shape C x H x W
-        img_path = '{}/{}/{}'.format(self.config.dataset.testsets_path, testset, img)
-        # print(img_path)
-        image = bob.io.base.load(img_path)
-
-        # Fix for handling images from the ten crop dataset (as they have different naming)
-        if img[1] == '_':
-            img = img[2:]
+    def _image_and_label_to_tensor(self, img_name, testset):
+        orig_image, img_name = self._load_image(img_name, testset)
 
         # Reshape to a numpy array of shape H x W x C
-        image = np.transpose(image, (1, 2, 0))
+        image = np.transpose(orig_image, (1, 2, 0))
 
         # convert each pixel to uint8
         image = image.astype(np.uint8)
 
         # to_tensor normalizes the numpy array (HxWxC) in the range [0. 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0]
-        image = to_tensor(image)
+        image = normalize(to_tensor(image), mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
         # Get true Y label for image
-        y = np.array(self.labels.loc[img].array)
+        y = np.array(self.labels.loc[img_name].array)
 
         # -1 --> 0
         y = np.where(y < 0, 0, y)
 
-        return image, torch.Tensor(y)
+        return image, torch.Tensor(y), orig_image
+
+    def _load_image(self, img_name, testset):
+        # Load image from disk yields numpy array of shape C x H x W
+        img_path = '{}/{}/{}'.format(self.config.dataset.testsets_path, testset, img_name)
+
+        img_name = img_name[:-3] + 'jpg'
+        # print(img_path)
+        image = bob.io.base.load(img_path)
+        return image, img_name
 
     def _get_attribute_baseline_accuracy_val(self, labels, config):
         train_attribute_baseline_majority_value = pd.read_pickle(config.dataset.majority_class_file,

generate_test_datasets.py
@@ -1,3 +1,5 @@
+import random
+
 import bob
 from PIL import Image
 from facenet_pytorch.models.mtcnn import MTCNN
@@ -29,6 +31,7 @@ def main():
     print('Creating testset AM (aligned, manual)')
     data_transforms_AM = transforms.Compose([AffactTransformer(config)])
     _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bounding_boxes, data_transforms_AM)
+
     # Define transformations for dataset AA (aligned, automatically)
     # Consists of images aligned according to the face detector
     print('Creating testset AA (aligned, automatically)')
@@ -38,9 +41,8 @@ def main():
     # face detector -> grösser bbx -> 10crop
     config = get_config('dataset/testsetC_config')
     print('Creating testset C (10 crop)')
-    data_transforms_C = transforms.Compose([AffactTransformer(config), TenCrop(224)])
-    _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bounding_boxes, data_transforms_C,
-                       uses_tencrop=True)
+    data_transforms_C = transforms.Compose([AffactTransformer(config)])
+    _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bounding_boxes, data_transforms_C)
     # face detector -> bigger bbx -> AFFACT Transformations
     config = get_config('dataset/testsetT_config')
     print('Creating testset T (AFFACT transformations)')
@@ -55,7 +57,8 @@ def main():
 
 
 
-def _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bounding_boxes, transformer, uses_tencrop=False):
+
+def _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bounding_boxes, transformer):
     """
     Generates test images based on dataframes
 
@@ -74,7 +77,7 @@ def _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bound
 
     # TODO
     # create_directory(config.dataset_result_folder, recreate=True)
-    create_directory(config.dataset_result_folder)
+    create_directory(config.dataset_result_folder, recreate=True)
     print("created {}".format(config.dataset_result_folder))
     pbar = tqdm(range(len(df_test_labels.index)))
     # pbar.clear()
@@ -101,7 +104,7 @@ def _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bound
                 bounding_boxes[2] = bounding_boxes[2] - bounding_boxes[0]
                 bounding_boxes[3] = bounding_boxes[3] - bounding_boxes[1]
             except:
-                print(row.name)
+                # print(row.name)
                 bounding_boxes = df_test_bounding_boxes.iloc[index].tolist()
                 bounding_boxes = bounding_boxes[1:]
 
@@ -119,18 +122,17 @@ def _create_test_images(config, df_test_labels, df_test_landmarks, df_test_bound
         }
         X = transformer(input)
 
-        if not uses_tencrop:
-            img = tensor_to_image(X)
-            img.save('{}/{}'.format(config.dataset_result_folder, row.name))
-        else:
-            for i in range(10):
-                img = tensor_to_image(X[i])
-                img.save('{}/{}_{}'.format(config.dataset_result_folder, i, row.name))
+
+        img = tensor_to_image(X)
+        img.save('{}/{}'.format(config.dataset_result_folder, row.name[:-3] + 'png'))
+
 
         pbar.update(1)
     pbar.close()
 
 
 if __name__ == '__main__':
+    random.seed(a=0, version=2)
+    np.random.seed(0)
     main()
 

preprocessing/affact_transformer.py
@@ -1,13 +1,12 @@
+import math
+
 import bob.io.image
 import bob.ip.base
 import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-import torch
 import random
 
-from torchvision.transforms import transforms
 from torchvision.transforms.functional import to_tensor, normalize
 
 from evaluation.utils import save_input_transform_output_image
@@ -71,7 +70,7 @@ class AffactTransformer():
             #     # bob.io.base.save(im, 'error{}.jpg'.format(index))
             #     # print('error')
             # else:
-            alpha = np.arctan2((t_eye_right[1] - t_eye_left[1]), (t_eye_right[0] - t_eye_left[0]))
+            alpha = math.degrees(np.arctan2((t_eye_right[1] - t_eye_left[1]), (t_eye_right[0] - t_eye_left[0])))
 
             bbx = [t_eye[0] - 0.5 * w,
                    t_eye[1] - 0.45 * h,
@@ -183,4 +182,4 @@ class AffactTransformer():
         placeholder_out = placeholder_out.astype(np.uint8)
 
         # to_tensor normalizes the numpy array (HxWxC) in the range [0. 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0]
-        return normalize(to_tensor(placeholder_out), mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        return to_tensor(placeholder_out)