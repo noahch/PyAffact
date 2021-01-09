895adae0edbd6b7e051e83ee13f52d6eca0fae24
yrutis <rutishauseryves@gmail.com>
2021-01-09 12:48:31 +0100

    ray tune hyperopt

---
+++ py_affact_train_hyperopt.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+"""
+Main File to start hyperopt the AFFACT Network
+"""
+import os
+
+from ray import tune
+from ray.tune.schedulers import ASHAScheduler
+from ray.tune import CLIReporter
+
+
+from utils.config_utils import get_config, create_result_directory
+from training.train_model import TrainModel
+from utils.utils import init_environment
+from functools import partial
+import os
+
+
+
+def main():
+    """
+    Run training for a specific model
+    Returns
+    -------
+
+    """
+    # Load configuration for training
+    config = get_config('train/affact_hyperopt')
+    # Init environment, use GPU if available, set random seed
+    device = init_environment(config)
+    # Create result directory
+    create_result_directory(config)
+    # Create a training instance with the loaded configuration on the loaded device
+    # training_instance = TrainModel(config, device)
+    # Run the training
+    # training_instance.train()
+
+    prefix = "/home/yves/Desktop/uzh/affact/PyAffact/"
+    print(prefix + config.dataset.dataset_labels_filename)
+
+
+    def train_hyperopt(config, data_dir=None):
+        config.basic.result_directory = prefix + "results"
+        config.dataset.dataset_labels_filename = prefix + config.dataset.dataset_labels_filename
+        config.dataset.partition_filename = prefix + config.dataset.partition_filename
+        config.dataset.dataset_image_folder = prefix + config.dataset.dataset_image_folder
+        config.dataset.landmarks_filename = prefix + config.dataset.landmarks_filename
+        config.dataset.bounding_boxes_filename = prefix + config.dataset.bounding_boxes_filename
+        training_instance = TrainModel(config, device)
+        # Run the training
+        training_instance.train_resnet_51_hyperopt()
+
+
+    data_dir = os.path.abspath("./data_dir")
+    config.preprocessing.dataloader.batch_size = tune.choice([2, 4, 8, 16])
+    # config.training.epochs = tune.choice([1, 2])
+
+    scheduler = ASHAScheduler(
+        metric="loss",
+        mode="min",
+        max_t=10,
+        grace_period=1,
+        reduction_factor=2)
+    reporter = CLIReporter(
+        # parameter_columns=["l1", "l2", "lr", "batch_size"],
+        metric_columns=["loss", "accuracy", "training_iteration"])
+    print("starting")
+    result = tune.run(
+        partial(train_hyperopt),
+        resources_per_trial={"cpu": 2, "gpu": 1},
+        config=config,
+        num_samples=2,
+        scheduler=scheduler,
+        progress_reporter=reporter)
+
+    best_trial = result.get_best_trial("loss", "min", "last")
+    print("Best trial config: {}".format(best_trial.config))
+    print("Best trial final validation loss: {}".format(
+        best_trial.last_result["loss"]))
+    print("Best trial final validation accuracy: {}".format(
+        best_trial.last_result["accuracy"]))
+
+    # best_trained_model = Net(best_trial.config["l1"], best_trial.config["l2"])
+    # device = "cpu"
+    # if torch.cuda.is_available():
+    #     device = "cuda:0"
+    #     if gpus_per_trial > 1:
+    #         best_trained_model = nn.DataParallel(best_trained_model)
+    # best_trained_model.to(device)
+    #
+    # best_checkpoint_dir = best_trial.checkpoint.value
+    # model_state, optimizer_state = torch.load(os.path.join(
+    #     best_checkpoint_dir, "checkpoint"))
+    # best_trained_model.load_state_dict(model_state)
+    #
+    # test_acc = test_accuracy(best_trained_model, device)
+    # print("Best trial test set accuracy: {}".format(test_acc))
+
+
+
+if __name__ == '__main__':
+    main()
--- training/train_model.py
+++ training/train_model.py
@@ -13,6 +13,9 @@ from training.model_manager import ModelManager
 import wandb
 import logging
 from tqdm import tqdm
+from ray import tune
+from ray.tune import CLIReporter
+from ray.tune.schedulers import ASHAScheduler
 
 
 # TODO: Extend from base class
@@ -263,6 +266,193 @@ class TrainModel(ModelManager):
                     epoch_accuracy_store_dict['val'] = epoch_attributes_correct_count_dict['val'].unsqueeze(0).clone()
 
             pbar.close()
+
+        time_elapsed = time.time() - since
+        logging.info('Training complete in {:.0f}m {:.0f}s'.format(
+            time_elapsed // 60, time_elapsed % 60))
+        logging.info('Best val Acc: {:4f}'.format(best_acc))
+
+        self.model.load_state_dict(best_model_wts)
+        self._save_model(best_model_wts, best_opt_wts, 'best-{}.pt'.format(best_epoch))
+        self.config.evaluation.model_weights_filename = 'best-{}.pt'.format(best_epoch)
+        save_config_to_file(self.config)
+
+        self._save_model(copy.deepcopy(self.model.state_dict()), copy.deepcopy(self.optimizer.state_dict()),
+                         'latest.pt')
+        if self.config.basic.enable_wand_reporting:
+            torch.save(self.model_device.state_dict(), os.path.join(wandb.run.dir, 'model_wand.pt'))
+        return self.model
+
+    def train_resnet_51_hyperopt(self):
+        if self.config.basic.enable_wand_reporting:
+            wandb.init(project="pyaffact_uzh", entity="uzh", name=self.config.basic.result_directory_name,
+                       notes=self.config.basic.experiment_description, config=self.config.toDict())
+
+            wandb.watch(self.model_device)
+
+        since = time.time()
+
+        best_model_wts = copy.deepcopy(self.model.state_dict())
+        best_opt_wts = copy.deepcopy(self.optimizer.state_dict())
+        best_epoch = ''
+        best_acc = 0.0
+
+        epoch_accuracy_loss_dict = {
+            'train': {
+                'accuracy': None,
+                'loss': None,
+            },
+            'val': {
+                'accuracy': None,
+                'loss': None,
+            }
+        }
+
+        epoch_accuracy_store_dict = {
+            'train': None,
+            'val': None
+        }
+
+        epoch_attributes_correct_count_dict = {
+            'train': None,
+            'val': None
+        }
+
+        for epoch in range(self.config.training.epochs):
+
+            logging.info('Epoch {}/{}'.format(epoch + 1, self.config.training.epochs))
+            print('Epoch {}/{}'.format(epoch + 1, self.config.training.epochs))
+
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    self.model.train()  # Set model to training mode
+                else:
+                    self.model.eval()  # Set model to evaluate mode
+
+                attributes_correct_count = torch.zeros(self.datasets['dataset_meta_information']['number_of_labels'])
+                attributes_correct_count = attributes_correct_count.to(self.device)
+                epoch_attributes_correct_count_dict[phase] = attributes_correct_count
+                running_loss = 0.0
+                correct_classifications = 0
+
+                pbar = tqdm(range(self.datasets['dataset_sizes'][phase]))
+
+
+                # Iterate over data.
+                for inputs, labels, _ in self.datasets['dataloaders'][phase]:
+                    # if phase == 'train':
+                    pbar.update(n=inputs.shape[0])
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+
+                    # zero the parameter gradients
+                    self.optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = self.model(inputs)  # TODO refactor to preds or predictions
+
+                        loss = self.criterion(outputs, labels.type_as(outputs))
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            self.optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+
+                    outputs[outputs < 0.5] = 0
+                    outputs[outputs >= 0.5] = 1
+                    # running_diff += torch.sum(prediction_loss)
+
+                    epoch_attributes_correct_count_dict[phase] += torch.sum(outputs == labels.type_as(outputs), dim=0)
+                    correct_classifications += torch.sum(outputs == labels.type_as(outputs))
+                    # print(correct_classifications)
+
+                if phase == 'val':
+                    if self.config.training.lr_scheduler.type == "ReduceLROnPlateau":
+                        self.lr_scheduler.step(loss)
+                        if self.lr_scheduler.in_cooldown:
+
+                            logging.info(
+                                "Changed learning rate from {} to {}. Reinitializing model weights with best model from epoch {}".format(
+                                    (1 / self.config.training.lr_scheduler.gamma) *
+                                    self.optimizer.param_groups[0]["lr"],
+                                    self.optimizer.param_groups[0]["lr"],
+                                    best_epoch))
+                            self.model_device.load_state_dict(best_model_wts)
+                    else:
+                        self.lr_scheduler.step()
+
+                epoch_loss = running_loss / self.datasets['dataset_sizes'][phase]
+                epoch_accuracy = correct_classifications.double(
+                ) / (self.datasets['dataset_sizes'][phase] * self.datasets['dataset_meta_information']['number_of_labels'])
+                # epoch_acc = running_diff / (self.datasets['dataset_sizes'][phase] * 40)
+                epoch_attributes_correct_count_dict[phase] = epoch_attributes_correct_count_dict[phase] / \
+                                                             self.datasets['dataset_sizes'][phase]
+
+                epoch_accuracy_loss_dict[phase]['accuracy'] = epoch_accuracy
+                epoch_accuracy_loss_dict[phase]['loss'] = epoch_loss
+
+                logging.info('{} Loss: {:.4f} \t Acc: {:.4f}'.format(phase, epoch_loss, epoch_accuracy))
+
+                # deep copy the model
+                if phase == 'val' and epoch_accuracy > best_acc:
+                    best_acc = epoch_accuracy
+                    best_model_wts = copy.deepcopy(self.model.state_dict())
+                    best_opt_wts = copy.deepcopy(self.optimizer.state_dict())
+                    best_epoch = epoch + 1
+
+                if phase == 'val' and (epoch + 1) % self.config.training.save_frequency == 0:
+                    self._save_model(copy.deepcopy(self.model.state_dict()), copy.deepcopy(self.optimizer.state_dict()),
+                                     '{:03d}.pt'.format(epoch + 1))
+
+            # Generating accuracy for each attribute to generate an epoch accuracy store in the form of [[acc_attr1, 2...], [epoch2_acc_attr1, 2...]]
+            # [[acc_attr1, 2...], [epoch2_acc_attr1, 2...]] --> transpose [[epoch1_acc_attr1, epoch2_acc_attr1...], [...]] to generate an per attribute accuracy progress
+            if self.config.basic.enable_wand_reporting:
+                wandb.log({
+                    "Accuracy Train": epoch_accuracy_loss_dict['train']['accuracy'],
+                    "Accuracy Val": epoch_accuracy_loss_dict['val']['accuracy'],
+                    "Loss Train": epoch_accuracy_loss_dict['train']['loss'],
+                    "Loss Val": epoch_accuracy_loss_dict['val']['loss'],
+                    "Baseline Train": self.datasets['attribute_baseline_accuracy']['train'].mean(),
+                    "Baseline Val": self.datasets['attribute_baseline_accuracy']['val'].mean()
+                }, step=epoch)
+
+                if epoch_accuracy_store_dict['train'] is not None:
+                    epoch_accuracy_store_dict['train'] = torch.cat(
+                        (epoch_accuracy_store_dict['train'], epoch_attributes_correct_count_dict['train'].unsqueeze(0)))
+                    epoch_accuracy_store_dict['val'] = torch.cat(
+                        (epoch_accuracy_store_dict['val'], epoch_attributes_correct_count_dict['val'].unsqueeze(0)))
+                    transposed_train = torch.transpose(epoch_accuracy_store_dict['train'], 0, 1).cpu()
+                    transposed_val = torch.transpose(epoch_accuracy_store_dict['val'], 0, 1).cpu()
+                    for i in range(0, self.datasets['dataset_meta_information']['number_of_labels']):
+                        attr_name = self.datasets['dataset_meta_information']['label_names'][i]
+                        fig = generate_attribute_accuracy_plot(attr_name,
+                                                               transposed_train[i].tolist(),
+                                                               self.datasets['attribute_baseline_accuracy']['train'][attr_name],
+                                                               transposed_val[i].tolist(),
+                                                               self.datasets['attribute_baseline_accuracy']['val'][attr_name],
+                                                               )
+                        wandb.log({'Accuracy {}'.format(attr_name): fig}, step=epoch)
+                else:
+                    epoch_accuracy_store_dict['train'] = epoch_attributes_correct_count_dict['train'].unsqueeze(
+                        0).clone()
+                    epoch_accuracy_store_dict['val'] = epoch_attributes_correct_count_dict['val'].unsqueeze(0).clone()
+
+            pbar.close()
+
+            with tune.checkpoint_dir(epoch) as checkpoint_dir:
+                path = os.path.join(checkpoint_dir, "checkpoint")
+                torch.save((self.model.state_dict(), self.optimizer.state_dict()), path)
+
+
+            tune.report(loss=(epoch_accuracy_loss_dict['val']['loss']), accuracy=epoch_accuracy_loss_dict['val']['accuracy'])
+
+
 
         time_elapsed = time.time() - since
         logging.info('Training complete in {:.0f}m {:.0f}s'.format(
